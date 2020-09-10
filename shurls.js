4ba964ed87dfdabcfee8402b875a35c9d3e1b59c
Angus Alves <angustalves@gmail.com>
2020-09-09 18:55:58 -0700

    fixed mongo upgrade / migration to atlas

--- shurls.js
+++ shurls.js
@@ -6,28 +6,29 @@ var auto = require('mongodb-autoincrement');
 var Shurls = {};
 
 Shurls.createShurls = function(callback){
-    mongo.connect(mongoURL,function(err,db){
+    mongo.connect(mongoURL,function(err,client){
         if(err){
             console.log("[-]ShurlClient.createShurls: Failed to connect to mongodb on url: "+mongoURL);
             console.log(err);
             callback(err);
         }else{
+            var db = client.db('urlshrinker');
             db.createCollection("shurls",{"capped":true,"size":1000000,"max":10000},function(err,coll){
                 if(err){
                     console.log("[-]ShurlClient.createShurls: Failed to create shurls collection");
                     console.log(err);
-                    db.close();
+                    client.close()
                     callback(err);
                 }else{
                     coll.createIndex({shurl:1},{unique:true},function(err,result){
                         if(err){
                             console.log("[-]ShurlClient.createShurls: Failed to create shurl index");
                             console.log(err);
-                            db.close();
+                            client.close()
                             callback(err,result);
                         }else{
                             console.log(result);
-                            db.close();
+                            client.close()
                             callback(err,result);
                         }
                     });
@@ -38,12 +39,13 @@ Shurls.createShurls = function(callback){
 };
 
 Shurls.createShurl = function(url,callback){
-    mongo.connect(mongoURL,function(err,db){
+    mongo.connect(mongoURL,function(err,client){
         if(err){
             console.log("[-]ShurlClient.createShurl: Failed to connect to mongodb on url: "+mongoURL);
             console.log(err);
             callback(err);
         }else{
+            var db = client.db('urlshrinker');
             auto.getNextSequence(db,'shurls','shurl',function(err,nextShurl){
                 if(err){
                     console.log("[-]ShurlClient.createShurl: Failed to get next shurl");
@@ -54,12 +56,12 @@ Shurls.createShurl = function(url,callback){
                         if(err){
                             console.log("[-]ShurlClient.createShurl: Failed to create shurl");
                             console.log(err);
-                            db.close();
+                            client.close()
                             callback(err,result);
                         }else{
                             console.log("[+]ShurlClient.createShurl: created shurl");
                             console.log(result);
-                            db.close();
+                            client.close()
                             callback(err,result);
                         }
                     });
@@ -70,24 +72,25 @@ Shurls.createShurl = function(url,callback){
 };
 
 Shurls.getShurl = function(fshurl,callback){
-    mongo.connect(mongoURL,function(err,db){
+    mongo.connect(mongoURL,function(err,client){
         if(err){
             console.log("[-]ShurlClient.createShurl: Failed to connect to mongodb on url: "+mongoURL);
             console.log(err);
             callback(err);
         }else{
+            var db = client.db('urlshrinker');
             var q = {shurl:fshurl};
             console.log(q);
             db.collection("shurls").findOne(q,function(err,result){
                 if(err||!result){
                     console.log("[-]ShurlClient.createShurl: Failed to find shurl");
                     console.log(err);
-                    db.close();
+                    client.close()
                     callback(err,result);
                 }else{
                     console.log("[+]ShurlClient.createShurl: found shurl");
                     console.log(result);
-                    db.close();
+                    client.close()
                     callback(err,result);
                 }
             });
